d30a201820a9009abe615ed27c0f660bf56ed735
2vyy <83983150+2vyy@users.noreply.github.com>
2026-01-09 09:38:30 -0600

    add requirements.txt

--- server_node/config.py
+++ server_node/config.py
@@ -9,10 +9,3 @@ settings.setValue("FRAME_THROTTLE_RATE", 1)
 settings.setValue("CAMERA_WIDTH", 0)
 settings.setValue("CAMERA_HEIGHT", 0)
 settings.setValue("RECORDING_TOGGLE", False)
-
-# Camera sources - list of (camera_id, camera_node_url) tuples
-# For local testing, use localhost. In production, use real IPs.
-CAMERA_SOURCES = [
-    (0, "http://localhost:8000"),
-    # (1, "http://192.168.1.101:8000"),
-]
